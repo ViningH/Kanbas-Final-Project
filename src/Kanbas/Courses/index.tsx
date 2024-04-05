b7e32de0d0ca11dc3041e4a87ae09c58cf0fbafb
Hannah <vining.h@husky.neu.edu>
2024-04-04 23:07:36 -0400

    fixed issues with package

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -15,7 +15,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 function Courses() {
     const { courseId } = useParams();
-    const COURSES_API = "https://kanbas-node-server-app-hv-05fc8772bf2c.herokuapp.com/api/courses";
+    const API_BASE = process.env.REACT_APP_API_BASE;
+    const COURSES_API = `${API_BASE}/api/courses`;
     const [course, setCourse] = useState<any>({ _id: "" });
     const findCourseById = async (courseId?: string) => {
         const response = await axios.get(
