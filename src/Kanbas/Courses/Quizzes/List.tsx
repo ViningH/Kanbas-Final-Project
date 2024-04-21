8580ef8b3cd27a531addf0b0db43f8bbdc31ca6c
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-04-21 11:45:48 -0700

    quizlist

--- src/Kanbas/Courses/Quizzes/List.tsx
+++ src/Kanbas/Courses/Quizzes/List.tsx
@@ -61,6 +61,19 @@ function QuizList() {
                                 <div className="wd-assignment-item-padding">
                                     <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></div>
                             </div>
+
+                            <div className="d-flex">
+                                <div className="wd-assignment-item-padding">
+                                    <FaRocket className="wd-green-pencil" />
+                                </div>
+                                <div className="flex-fill wd-quiz-text-padding">
+                                    <h4><Link to={`/Kanbas/Courses/${courseId}/Quizzes/${quiz._id}/Details`} className="nav-link">{quiz.title}</Link></h4>
+                                    <span><strong>Closed</strong></span> | <strong>Due</strong> Sep 21 at 1 pm |
+                                    29 pts | 11 Questions
+                                </div>
+                                <div className="wd-assignment-item-padding">
+                                    <FaCheckCircle className="text-success" /><FaEllipsisV className="ms-2" /></div>
+                            </div>
                         </li>
                         {/* ))} */}
                     </ul>
